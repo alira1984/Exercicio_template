d614e0ef523e9feaff69805cc7c4b16ef561ee67
alira1984 <anderlira@yahoo.com.br>
2022-09-08 09:26:15 -0300

    primeiro commit

---
+++ templates_ejs/src/models/modelos.js
@@ -0,0 +1,21 @@
+class Projeto{
+    constructor(tipo, tecnologia, inicio, fim, github){
+        this.tipo = tipo;
+        this.tecnologia = tecnologia;
+        this.inicio = inicio;
+        this.fim = fim;
+        this.github = github;
+
+    }
+}
+
+
+
+module.exports = {
+    Projeto: Projeto
+}  
+
+
+
+
+
